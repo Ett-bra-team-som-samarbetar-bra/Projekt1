aa595ab6933683abdcbedeb371777c65c68709a8
Joakim Bjerselius <joakim.bjerselius@hotmail.com>
2024-09-28 07:01:32 +0200

    add class and method

---
+++ DiceGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt1
+{
+    public class DiceGame
+    {
+        private string? playerOne;
+        private string? playerTwo;
+        private int playerOnePoints = 0;
+        private int playerTwoPoints = 0;
+
+        public void InitializePlayers()
+        {
+            Console.Clear();
+            System.Console.WriteLine("Enter name of player one: ");
+            playerOne = Console.ReadLine()!;
+            System.Console.WriteLine("Enter name of player two: ");
+            playerTwo = Console.ReadLine()!;
+
+        }
+
+    }
+
+
+}
