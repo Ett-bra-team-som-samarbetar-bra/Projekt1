cd16672ae6a2e70328dd4cb72935e28a2c3d3412
Joakim Bjerselius <176925453+jockeBjers@users.noreply.github.com>
2024-09-29 10:57:53 +0200

    Add functionality to a disfunctional program

--- Program.cs
+++ Program.cs
@@ -6,29 +6,38 @@ namespace Projekt1;
 class Program
 {
     // Entry point va
-    public static DiceGame diceGame = new DiceGame();
+    public static TheGenerator theGenerator = new();
     static void Main(string[] args)
     {
-
-        Console.WriteLine("Jesper take over VA");
-        int x = 10;
-        Console.WriteLine(x);
-        Console.WriteLine("VAR ÄR KOMMENTARERNA?!?!?!!?!!?");
-        x = 10;
-        Console.WriteLine("Något annat än gorgonzola");
-
-        Console.WriteLine("Gorgonzola");
-        PlayGame();
-        
+        Generator();
     }
 
-    public static void PlayGame()
+    public static void Generator()
     {
-        diceGame.InitializePlayers();
+        Random random = new Random();
+        // Randomly pick which category to use: 0 for insults, 1 for motivation, 2 for love poems
+        int randomPick = random.Next(0, 3);
 
+        switch (randomPick)
+        {
+            case 0:
+                InsultGenerator();
+                break;
+            case 1:
+                Console.WriteLine("Random Motivation: ");
+                break;
+            case 2:
+                Console.WriteLine("Random Love Poem: ");
+                break;
+
+        }
     }
 
-
-
+    public static void InsultGenerator()
+    {
+        theGenerator.GenerateInsult();
+    }
+    //Add more methods
+    
 
 }
